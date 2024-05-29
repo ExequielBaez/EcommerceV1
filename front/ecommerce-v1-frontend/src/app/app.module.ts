f45e369cfd39db3ca099818d7243274c20e5a8f9
ExequielBaez <exequielbaez@gmail.com>
2024-05-29 16:07:03 -0300

    feat:cart-image-data

--- front/ecommerce-v1-frontend/src/app/app.module.ts
+++ front/ecommerce-v1-frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { CategoryListComponent } from './components/category/category-list/category-list.component';
 import { CategoryAddComponent } from './components/category/category-add/category-add.component';
 import { DetailProductComponent } from './components/cart/detail-product/detail-product.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
 
 
 const routes : Routes = [
@@ -36,7 +37,8 @@ const routes : Routes = [
     ProductAddComponent,
     CategoryListComponent,
     CategoryAddComponent,
-    DetailProductComponent
+    DetailProductComponent,
+    HeaderUserComponent
   ],
   imports: [
     BrowserModule,
--- front/ecommerce-v1-frontend/src/app/components/cart/detail-product/detail-product.component.ts
+++ front/ecommerce-v1-frontend/src/app/components/cart/detail-product/detail-product.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-detail-product',
   templateUrl: './detail-product.component.html',
   styleUrls: ['./detail-product.component.css']
 })
-export class DetailProductComponent {
+export class DetailProductComponent implements OnInit{
 
+  idProduct : string = '';
+  nameProduct : string = '';
+  description : string = '';
+  price : number = 0;
+  urlImage : string = '';
+  quantity : number = 0;
+
+  ngOnInit(): void {
+    this.getProductById();
+    
+  }
+
+  constructor(private productService:ProductService, 
+              private activatedRoute:ActivatedRoute,
+              private cartService:CartService){
+  }
+
+  getProductById(){
+    this.activatedRoute.params.subscribe(
+      p => {
+        let id = p['id'];
+        if(id){
+          this.productService.getProductById(id).subscribe(
+            data => {
+              this.idProduct = data.idProduct;
+              this.nameProduct = data.nameProduct;
+              this.description = data.description;
+              this.urlImage = data.urlImage;
+              this.price = parseInt(data.price);
+
+            }
+          );
+        }
+      }
+    );
+  }
+
+  addCart(id : string){
+    console.log('id product', this.idProduct);
+    console.log('name product', this.nameProduct);
+    console.log('price product', this.price);
+    console.log('quantity', this.quantity);
+
+  }
 }
